a59361b07f813d083d3dd63242fbdb64a1755359
Amer <ameralghfili@gmail.com>
2020-01-16 05:14:49 +0300

    second

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,23 +1,19 @@
-if ("serviceWorker" in navigator) {
-    window.addEventListener("load", function() {
-      navigator.serviceWorker.register("/serviceWorker.js").then(
-        function(registration) {
-          console.log(
-            "ServiceWorker registration successful with scope: ",
-            registration.scope
-          );
-        },
-        function(err) {
-          console.log("ServiceWorker registration failed: ", err);
-        }
-      );
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
     });
-  }
-  
-  var staticCacheName = "project-static-v2";
-  var urlsToCache = [
-    "/",
-    "/index.html",
+  });
+}
+
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+  '/',
+  "/index.html",
     "/restaurant.html",
     "/css/styles.css",
     "/css/indexBreakpoints/over625.css",
@@ -38,44 +34,65 @@ if ("serviceWorker" in navigator) {
     "/img/8.jpg",
     "/img/9.jpg",
     "/img/10.jpg"
-  ];
-  
-  self.addEventListener("install", function(event) {
-    event.waitUntil(
-      caches.open(staticCacheName).then(function(cache) {
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
         return cache.addAll(urlsToCache);
       })
-    );
-  });
-  
-  self.addEventListener("activate", function(event) {
-    event.waitUntil(
-      caches.keys().then(function(cacheNames) {
-        return Promise.all(
-          cacheNames
-            .filter(function(cacheName) {
-              return (
-                cacheName.startsWith("project-") && cacheName != staticCacheName
-              );
-            })
-            .map(function(cacheName) {
-              return caches.delete(cacheName);
-            })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+
+        return fetch(event.request).then(
+          function(response) {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+            // IMPORTANT: Clone the response. A response is a stream
+            // and because we want the browser to consume the response
+            // as well as the cache consuming the response, we need
+            // to clone it so we have two streams.
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
         );
       })
     );
-  });
-  
-  self.addEventListener("fetch", function(event) {
-    event.respondWith(
-      caches.match(event.request).then(function(response) {
-        return response || fetch(event.request);
-      })
-    );
-  });
-  
-  self.addEventListener("message", function(event) {
-    if (event.data.action === "skipWaiting") {
-      self.skipWaiting();
-    }
-  });
+});
+
+self.addEventListener('activate', function(event) {
+
+  var cacheWhitelist = ['pages-cache-v1', 'blog-posts-cache-v1'];
+
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
